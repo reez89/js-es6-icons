f194dfbc04f052a4de55405817f8192cc37880a8
Nicola R <nicola.rizzi89@hotmail.it>
2020-12-14 09:29:10 +0100

    Finished
    Different solutions.

--- milestone3/main.js
+++ milestone3/main.js
@@ -31,16 +31,29 @@ $(document).ready(function () {
      const purple = 'purple';
    
      //aggiungiamo dei colori come da milestone 2
-     const colorsArray = myIcons.map(element =>{
-        if (element.family === 'animali'){
-            var color = blue;
-        } else if (element.family === 'vegetables'){
-            var color = orange;
-        }else {
-            var color = purple;
-        }
 
-        const newColorsArray = {...element, color: color}
+     const colorsArray = myIcons.map(element =>{
+         
+        switch (true) { // PROVATO CON UNO SWITCH CASE INVECE DI UN IF ELSE.
+             case element.family === 'animali':
+                var color = blue;
+                break;
+             case element.family === 'vegetables':
+                var color = orange;
+                 break;
+             case element.family === 'user':
+                var color = purple;
+                 break;
+         }
+        // if (element.family === 'animali'){
+        //     var color = blue;
+        // } else if (element.family === 'vegetables'){
+        //     var color = orange;
+        // }else {
+        //     var color = purple;
+        // }
+
+        const newColorsArray = {...element, color: color};
         
         return newColorsArray;
     });
@@ -69,23 +82,42 @@ $(document).ready(function () {
      select.append('<option value="user">Users</option>');
      //al change mostriamo solo le icone filtrate
      
-     const elementSelection = document.getElementById('type');
 
-     elementSelection.addEventListener('change', event => {
 
-        const choice = $('#type option:selected').val();
+     /* JQUERY VERSION */
+     select.change(function(){
+
+        const choice = $('#type option:selected').val(); 
 
         const newFilterArray = colorsArray.filter( i => i.family === choice);
 
-        container.innerHTML = '';
-
+        $('div.icons').empty(); // JQUERY VERSION
+        
         print(newFilterArray, container);
 
         if (choice == 'all'){
             print(colorsArray, container);
         }
+     });
+
+     /* VANILLA JAVASCRIPT */
+    //  const elementSelection = document.getElementById('type');
+
+    //  elementSelection.addEventListener('change', event => {
+
+    //     const choice = $('#type option:selected').val();
+
+    //     const newFilterArray = colorsArray.filter( i => i.family === choice);
+
+    //     container.innerHTML = '';
+
+    //     print(newFilterArray, container);
+
+    //     if (choice == 'all'){
+    //         print(colorsArray, container);
+    //     }
          
-     })
+    //  })
      //mostriamo come passare un parametro a change e contemporaneamente destrutturiamo
    
    
